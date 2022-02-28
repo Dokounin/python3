270b1c2ff784b2aceb36fdc87de205b4c5ccace3
dyacenko <dyacenkonikita@gmail.com>
2022-02-28 16:02:28 +0100

    Création du repo
    Ajout des premiers chapitre de cours.
    Ajout des exo correspondants.

---
+++ variables.py
@@ -0,0 +1,35 @@
+nombre = -123
+print(nombre)
+
+nombre = -3.14
+print(nombre)
+
+nombre = "-123"
+print(nombre)
+
+text = "lorem ipsum"
+print(text)
+
+is_day = True
+print(is_day)
+
+has_sugar = False
+print(has_sugar)
+
+has_accepted_ula = None
+print(has_accepted_ula)
+
+html_code = '<h1>Titre de mon blog</h1>'
+print(html_code)
+
+nickname = "Jonh \"Dead\" Doe"
+nickname = 'Jonh O\'Connar'
+print(nickname)
+
+multiline_text = """'foo'
+"bar"
+baz"""
+print(multiline_text)
+
+multiline_text = "foo \nbar \nbaz"
+print(multiline_text)
