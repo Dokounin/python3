a0a483e7675ebd768815c186298a1582f56e929c
dyacenko <dyacenkonikita@gmail.com>
2022-03-02 11:58:31 +0100

    Mise a jour pour variables
    Exploration de conversions de type.

--- variables.py
+++ variables.py
@@ -1,23 +1,30 @@
 nombre = -123
 print(nombre)
+print(type(nombre))
 
 nombre = -3.14
 print(nombre)
+print(type(nombre))
 
 nombre = "-123"
 print(nombre)
+print(type(nombre))
 
 text = "lorem ipsum"
 print(text)
+print(type(text))
 
 is_day = True
 print(is_day)
+print(type(is_day))
 
 has_sugar = False
 print(has_sugar)
+print(type(has_sugar))
 
 has_accepted_ula = None
 print(has_accepted_ula)
+print(type(has_accepted_ula))
 
 html_code = '<h1>Titre de mon blog</h1>'
 print(html_code)
@@ -33,3 +40,69 @@ print(multiline_text)
 
 multiline_text = "foo \nbar \nbaz"
 print(multiline_text)
+
+nombre = "5"
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = "2.71"
+print(type(nombre))
+nombre = float(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = 3.14
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = 3
+print(nombre)
+print(type(nombre))
+nombre = float(nombre)
+print(nombre)
+print(type(nombre))
+
+texte = str(nombre)
+print(texte)
+print(type(texte))
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 1
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = ""
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "-123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [123, "abc", False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = []
+my_var = bool(my_var)
+print(my_var)
